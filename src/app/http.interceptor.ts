70c4905b5c4da8da99405ee26071e92d6103b7ff
ssliteris <ssliteris@exadel.com>
2020-01-08 15:45:15 +0200

    fix(style): missing semicolumn

--- src/app/http.interceptor.ts
+++ src/app/http.interceptor.ts
@@ -1,5 +1,5 @@
 import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core'
+import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { MatSnackBar } from '@angular/material';
