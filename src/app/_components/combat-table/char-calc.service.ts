e4e70e5c0ea311175621e983c1332e2e795e07e3
deamon <deamongd@gmail.com>
2020-06-26 12:10:28 +0300

    (feat):Combat table completed

--- src/app/_components/combat-table/char-calc.service.ts
+++ src/app/_components/combat-table/char-calc.service.ts
@@ -10,7 +10,9 @@ export class CharCalcService {
 
   public calcEnc(char: Character) {
     char.enc = (char.headWeight + char.breastWeight +
-    char.lArmWeight + char.rArmWeight + char.lLegWeight + char.rLegWeight) / 6 + char.shieldWeight - char.bonusEnc;
+    char.lArmWeight + char.rArmWeight + char.lLegWeight + char.rLegWeight +
+    char.pHeadWeight + char.pBreastWeight + char.pLArmWeight + char.pLArmWeight +
+    char.pLLegWeight + char.pRArmWeight) / 6 + char.shieldWeight - char.bonusEnc;
     return Math.round(char.enc);
   }
   public calcMove(char: Character) {
@@ -22,8 +24,50 @@ export class CharCalcService {
     return Math.round(char.maxVigor);
   }
 
-  public calcVigorLeft(char: Character) {
-    char.vigorLeft = char.maxVigor - char.vigorSpent;
-    return Math.round(char.vigorLeft);
+  public calcCharHP(char: Character) {
+    char.HP = char.sta * 2;
+    return char.HP;
+  }
+
+  public calcHPLeft(char: Character) {
+    char.HPLeft = char.HP - ((char.dmgBH + char.dmgBB + char.dmgBLA + char.dmgBLL + char.dmgBRA + char.dmgBRL) * 2 +
+    (char.dmgTH + char.dmgTB + char.dmgTLA + char.dmgTLL + char.dmgTRA + char.dmgTRL) * 2 +
+    (char.dmgCH + char.dmgCB + char.dmgCLA + char.dmgCLL + char.dmgCRA + char.dmgCRL) * 3 +
+    (char.dmgFH + char.dmgFB + char.dmgFLA + char.dmgFLL + char.dmgFRA + char.dmgFRL) * 3);
+    return char.HPLeft;
+  }
+
+  public calcMinusAtt(char: Character) {
+    char.minusAtt = Math.floor(char.dmgTH / 2) + Math.floor(char.dmgFH / 2) +
+    Math.floor(char.dmgCB / 2) + Math.floor(char.dmgTLA / 2) + Math.floor(char.dmgTRA / 2) +
+    Math.floor(char.dmgTLA / 2) + Math.floor(char.dmgBLA / 2) + Math.floor(char.dmgBRA / 2) +
+    Math.floor(char.dmgCLA / 2) + Math.floor(char.dmgCRA / 2) + Math.floor(char.dmgFLA / 2) + Math.floor(char.dmgFRA / 2) +
+    Math.floor(char.penalty);
+    return char.minusAtt;
+  }
+
+  public calcMinusDef(char: Character) {
+    char.minusDef = Math.floor(char.dmgTH / 2) + Math.floor(char.dmgCH / 2) +
+    Math.floor(char.dmgFH / 2) + Math.floor(char.dmgTB / 2) + Math.floor(char.dmgCB / 2) +
+    Math.floor(char.dmgFB / 2) + Math.floor(char.dmgBLA / 2) + Math.floor(char.dmgBRA / 2) + Math.floor(char.penalty);
+    return char.minusDef;
+  }
+
+  public calcMinusMove(char: Character) {
+    char.minusMove = Math.floor(char.dmgTLL / 2) + Math.floor(char.dmgTRL / 2) +
+    Math.floor(char.dmgBLL / 2) + Math.floor(char.dmgBRL / 2) +
+    Math.floor(char.dmgCLL / 2) + Math.floor(char.dmgCRL / 2) +
+    Math.floor(char.dmgFLL / 2) + Math.floor(char.dmgFRL / 2);
+    return char.minusMove;
+  }
+
+  public calcPlusVigorCost(char: Character) {
+    char.plusVigorCost = Math.floor(char.dmgBH / 2) + Math.floor(char.dmgBB / 2);
+    return char.plusVigorCost;
+  }
+
+  public calcPenalty (char: Character) {
+    char.penalty = char.vigorSpent / char.maxVigor;
+    return Math.floor(char.penalty);
   }
 }
--- src/app/_components/combat-table/character.ts
+++ src/app/_components/combat-table/character.ts
@@ -20,6 +20,13 @@ export default class Character {
   rLegWeight = 0;
   shieldWeight = 0;
 
+  pHeadWeight = 0;
+  pBreastWeight = 0;
+  pLArmWeight = 0;
+  pRArmWeight = 0;
+  pLLegWeight = 0;
+  pRLegWeight = 0;
+
   headArmor = 0;
   breastArmor = 0;
   lArmArmor = 0;
@@ -31,7 +38,42 @@ export default class Character {
   move = 0;
   bonusEnc = 0;
   maxVigor = 0;
-  vigorLeft = 0;
+  HP = 0;
+  HPLeft = 0;
+
+  dmgTH = 0;
+  dmgTB = 0;
+  dmgTLA = 0;
+  dmgTRA = 0;
+  dmgTLL = 0;
+  dmgTRL = 0;
+
+  dmgBH = 0;
+  dmgBB = 0;
+  dmgBLA = 0;
+  dmgBRA = 0;
+  dmgBLL = 0;
+  dmgBRL = 0;
+
+  dmgCH = 0;
+  dmgCB = 0;
+  dmgCLA = 0;
+  dmgCRA = 0;
+  dmgCLL = 0;
+  dmgCRL = 0;
+
+  dmgFH = 0;
+  dmgFB = 0;
+  dmgFLA = 0;
+  dmgFRA = 0;
+  dmgFLL = 0;
+  dmgFRL = 0;
+
+  minusAtt = 0;
+  minusDef = 0;
+  minusMove = 0;
+  plusVigorCost = 0;
+  penalty = 0;
 
   constructor( name: string) {
     this.name = name;
